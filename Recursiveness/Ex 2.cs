b629a4481ef9632a17ae0d50a0a6090419bc55e1
Alessandra Faria <alessandra.faria.abreu@gmail.com>
2018-02-20 22:09:13 -0300

    Update Ex 2.cs

--- Recursiveness/Ex 2.cs
+++ Recursiveness/Ex 2.cs
@@ -6,53 +6,46 @@ using System.Threading.Tasks;
 
 namespace ex2
 {
-    class Program
+class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        int nu1 = 0, nu2 = 0, vezes = 1;
+
+        Console.WriteLine("Digite o Primeiro Número ");
+        nu1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Digite o Segundo Número ");
+        nu2 = Convert.ToInt32(Console.ReadLine());
+        if (nu2 == 0)
         {
-            int nu1 = 0, nu2 = 0, vezes = 1;
-
-            Console.WriteLine("Digite o Primeiro Número ");
-            nu1 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Digite o Segundo Número ");
-            nu2 = Convert.ToInt32(Console.ReadLine());
-            if (nu2 == 0)
-            {
-                Console.WriteLine("Não é possivel realizar divisão por 0 ");
-            }
-            else
-            {
-                Console.WriteLine("Resultado  " + nu1 + " / " + nu2 + " = " + Divisao(nu1, nu2, vezes));
-            }
-
-            Console.ReadKey();
+            Console.WriteLine("Não é possivel realizar divisão por 0 ");
         }
-        static public int Divisao(int n1, int n2,  int vezes)
+        else
         {
+            Console.WriteLine("Resultado  " + nu1 + " / " + nu2 + " = " + Divisao(nu1, nu2);
+        }
 
-            if ((n1 == 0))
-            {
-                return 0;
-            }
-            if (n1<n2)
-            {
-                return vezes;//=vezes-1;
-            }
+        Console.ReadKey();
+    }
+    static public int Divisao(int n1, int n2)
+    {
+
+        if ((n1 == 0))
+        {
+            return 0;
+        }
+        else if (n1<n2)
+        {
+            return 0;
+        }
             else
             {
-                n1 = n1 - n2;//(n2 * vezes);
-                if ((n1 < 0)||(n1==0)||(n1<n2))
-                {
-                    return vezes;
-                }
-                else
-                {
-                    return Divisao(n1, n2, (vezes + 1));
-                }
+                return 1+ Divisao((n1-n2), n2);
             }
         }
     }
+}
 }
 
 
